b21e41d0d8f1cf626746a12c89a7ff498e929300
Irvin Barreto <61056629+IrvinBarreto@users.noreply.github.com>
2020-10-20 12:45:24 -0500

    guardado final

--- clientes.js
+++ clientes.js
@@ -1,43 +1,117 @@
-const { Router} = require('express');
-const router = Router();
-const _ = require('underscore');
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+const app = express();
 
-const clientes = require('../csvjson.json');
-const users1 = require('../sample.json');
-//console.log(movies);
+app.set('view engine', 'ejs');
 
-router.get('/',(req, res)=>{
-    res.send(clientes);
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/HackatonBBVA", {
+  useNewUrlParser: true, useUnifiedTopology: true
 });
 
+const ClienteSchema = {
 
-router.post('/', (req,res)=>{
-    //console.log(req.body);
-    //const {title, director, year,rating}= req.body;
-    const {usuario, auth, idCliente}= req.body;
-    if (usuario && auth &&  idCliente){
-        _.each(users1,(user1,i)=>{
-            if(user1.usuario==usuario && user1.auth ==auth){
-                if(user1.perfil=="Manager"){
-                    res.send('manager');
-                } else if (user1.perfil=="Validador"){
-                    res.send('validador');
-                } else if (user1.perfil=="Restringido"){
-                    res.send('restringido');
-                }
-                
-                /*let prueba = user.director
-                if(user.director.length == 0){
-                    prueba = "vacio"
-                }
-                res.json(prueba);
-                res.send('siempre si');*/
-            } 
+  idCliente: String,
+  nombre: String,
+  apellidoPaterno: String,
+  apellidoMaterno: String,
+  fechaNacimiento: Date,
+  sexo: String,
+  segmento: String,
+  nacionalidad: String,
+  rfc: String,
+  tipoID: String,
+  numeroID: String,
+  cuenta: String,
+  email: String
+};
 
+const Cliente = mongoose.model("Cliente", ClienteSchema);
+
+
+
+const usuarioSchema = {
+
+  usuario: String,
+  auth: String,
+  nombreCompleto: String,
+  area: String,
+  ubicacion: Date,
+  segmento: Number,
+  perfil: String,
+
+};
+
+const Usuario = mongoose.model("Usuario", ClienteSchema);
+
+//Autenticacion
+
+app.route('/')
+
+.post( req , res => {
+
+    const { usuarioPedido, auth, idCliente } = req.body;
+
+    Usuario.findOne({ usuario: usuarioPedido }, function (err, usuarioEncontrado) {
+      if (!err) {
+        if (usuarioEncontrado) {
+          if (usuarioEncontrado.auth === auth) {
+            switch (usuarioEncontrado.perfil) {
+              case "Manager":
+                res.send("es manager")
+
+
+                break;
+              case "Validador":
+                res.send("es validador")
+
+                break;
+              case "Restringido":
+                res.send("es restringido")
+
+                break;
+
+              default:
+                break;
+            }
+          }
+        }
+      }
     });
-}
-   
+
+
+
+
 });
 
 
-module.exports = router;
+
+
+
+
+//Rutas de Clientes
+
+app.route("/clientes/:id")
+
+  .get(function (req, res) {
+
+    const clienteID = req.params.id;
+    Cliente.findOne({ idCliente: clienteID }, function (err, cliente) {
+      if (cliente) {
+        const jsonCliente = JSON.stringify(cliente);
+        res.send(jsonCliente);
+      } else {
+        res.send("No se encuentra el dato");
+      }
+    });
+  })
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
