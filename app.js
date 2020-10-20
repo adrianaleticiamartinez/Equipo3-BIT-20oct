bb0461839f3cd1d35e3ee066e4358318022e8077
Irvin Barreto <61056629+IrvinBarreto@users.noreply.github.com>
2020-10-20 11:42:12 -0500

    agregue app.js
    se agrego un archivo donde va estar el servidor

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/HackatonBBVA", {
+  useNewUrlParser: true,useUnifiedTopology: true
+});
+
+const ClienteSchema = {
+  
+  idCliente: String,
+  nombre: String,
+  apellidoPaterno: String,
+  apellidoMaterno: String,
+  fechaNacimiento: Date,
+  sexo: String,
+  segmento: CharacterData,
+  nacionalidad:String,
+  rfc:String,
+  tipoID: String,
+  numeroID: String,
+  cuenta: String,
+  email: String
+};
+
+const Cliente = mongoose.model("Cliente", ClienteSchema);
+
+
+
+
+app.route("/clientes/:id")
+
+.get(function(req, res){
+  
+  const clienteID = req.params.id;
+  Cliente.findOne({idCliente: clienteID}, function(err, cliente){
+    if (cliente){
+      const jsonCliente = JSON.stringify(cliente);
+      res.send(jsonCliente);
+    } else {
+      res.send("No se encuentra el dato");
+    }
+  });
+})
